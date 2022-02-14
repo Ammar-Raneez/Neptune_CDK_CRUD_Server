09e8ab42a8a5fbeb9241ff1eb144bb2b7c4ca654
ammar <ammarraneez@gmail.com>
2022-02-14 10:13:57 +0530

    Initial commit

---
+++ test/neptune-api-gateway.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NeptuneApiGateway from '../lib/neptune-api-gateway-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/neptune-api-gateway-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NeptuneApiGateway.NeptuneApiGatewayStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
