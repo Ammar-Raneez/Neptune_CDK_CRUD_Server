906073532c0abbe37f1949a5c1f6af90fd701ca9
ammar <ammarraneez@gmail.com>
2022-02-14 13:05:19 +0530

    feat: delete post

---
+++ lambdas/deletePost.ts
@@ -0,0 +1,35 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+import { driver, structure } from 'gremlin';
+
+const DriverRemoteConnection = driver.DriverRemoteConnection;
+const Graph = structure.Graph;
+const uri = process.env.WRITER;
+
+async function handler(event: APIGatewayProxyEvent, context: Context) {
+  const result: APIGatewayProxyResult = {
+    statusCode: 200,
+    body: '',
+  };
+
+  let postId;
+  if (event.pathParameters) {
+    postId = event.pathParameters.postId;
+  }
+
+  const driverConnector = new DriverRemoteConnection(`wss://${uri}/gremlin`, {});
+  const graph = new Graph();
+  const g = graph.traversal().withRemote(driverConnector);
+
+  try {
+    await g.V(postId).drop().iterate();
+    driverConnector.close();
+    result.body = JSON.stringify(postId);
+  } catch (err) {
+    console.log('ERROR', err);
+    result.body = (err as any).message;
+  }
+
+  return result;
+}
+
+export default handler;
